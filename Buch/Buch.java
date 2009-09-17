9b5aec8b9e94f0b74c7d7630847afb1e9ac647fa
Christoph Schüler <christoph.schueler@googlemail.com>
2009-09-17 18:33:02 +0200

    Projekt Buch hinzugefügt
    Neues Projekt in den Ordner Kopiert, keine Änderungen vorgenommen.

---
+++ Buch/Buch.java
@@ -0,0 +1,25 @@
+/**
+ * Eine Klasse, deren Exemplare Informationen über ein Buch halten.
+ * Dies könnte Teil einer größeren Anwendung sein, einer
+ * Bibliothekssoftware beispielsweise.
+ *
+ * @author (Ihren Namen hier eintragen.)
+ * @version (das heutige Datum eintragen.)
+ */
+class Buch
+{
+    // Exemplarvariablen
+    private String autor;
+    private String titel;
+
+    /**
+     * Setze den Autor und den Titel, wenn ein Exemplar erzeugt wird.
+     */
+    public Buch(String buchautor, String buchtitel)
+    {
+        autor = buchautor;
+        titel = buchtitel;
+    }
+
+    // weitere Methoden hier einfügen ...
+}
