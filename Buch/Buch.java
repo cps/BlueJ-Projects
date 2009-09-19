56e718ed6f15b6de8f04b1a6be54da8b5258ef2d
Christoph Schüler <christoph.schueler@googlemail.com>
2009-09-19 15:11:47 +0200

    Erste Methoden eingefügt
    Quelltext formatieren lassen und die ersten Methoden (sondierende) von dem Blatt zum Projekt eingefügt.

--- Buch/Buch.java
+++ Buch/Buch.java
@@ -6,20 +6,31 @@
  * @author (Ihren Namen hier eintragen.)
  * @version (das heutige Datum eintragen.)
  */
-class Buch
-{
-    // Exemplarvariablen
-    private String autor;
-    private String titel;
+class Buch {
+	// Exemplarvariablen
+	private String autor;
+	private String titel;
 
-    /**
-     * Setze den Autor und den Titel, wenn ein Exemplar erzeugt wird.
-     */
-    public Buch(String buchautor, String buchtitel)
-    {
-        autor = buchautor;
-        titel = buchtitel;
-    }
+	/**
+	 * Setze den Autor und den Titel, wenn ein Exemplar erzeugt wird.
+	 */
+	public Buch(String buchautor, String buchtitel) {
+		autor = buchautor;
+		titel = buchtitel;
+	}
 
-    // weitere Methoden hier einfügen ...
+	/**
+	 * Gib den Autor des Buches zurück.
+	 */
+	public String gibAutor() {
+		return autor;
+	}
+
+	/**
+	 * Gid den Titel des Buches zurück.
+	 */
+	public String gibTitel() {
+		return titel;
+	}
+
 }
