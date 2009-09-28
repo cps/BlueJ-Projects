99f9f08221db0c6444303e1e65e121c040c45710
Christoph Schüler <christoph.schueler@googlemail.com>
2009-09-28 20:08:21 +0200

    Weiterarbeit am Blatt in der Schule
    Fast alles fertig.

--- BMIWaage/Waage.java
+++ BMIWaage/Waage.java
@@ -3,49 +3,84 @@
  * BMI wird berechnet.
  */
 public class Waage {
-	private int gewicht;
-	private int groesse;
+	private double gewicht;
+	private double groesse;
+	private double bmi;
 
 	/**
 	 * Zwei Konstruktoren: 1. Mit Eingabe beider Werte,
 	 *                     2. Ohne Eingabe von Werten.
 	 */
-	public Waage(int neuesGewicht, int neueGroesse) {
+	public Waage(double neuesGewicht, double neueGroesse) {
 		gewicht = neuesGewicht;
 		groesse = neueGroesse;
+		bmi = 0;
 	}
 
 	public Waage() {
 		gewicht = 0;
 		groesse = 0;
+		bmi = 0;
 	}
 
 	/**
 	 * Gibt Gewicht zurück.
 	 */
-	public int gibGewicht() {
+	public double gibGewicht() {
 		return gewicht;
 	}
 
 	/**
 	 * Gibt Groesse zurück.
 	 */
-	public int gibGroesse() {
+	public double gibGroesse() {
 		return groesse;
 	}
 
 	/**
 	 * Verändert Gewicht
 	 */
-	public void setzeGewicht(int neuesGewicht) {
+	public void setzeGewicht(double neuesGewicht) {
 		gewicht = neuesGewicht;
 	}
 
 	/**
 	 * Verändert Größe.
 	 */
-	public void setzeGroesse(int neueGroesse) {
+	public void setzeGroesse(double neueGroesse) {
 		groesse = neueGroesse;
+	}
+
+	/**
+	 * Berechnet den BMI aus den vorher eingegeben Werten, falls sie gesetzt und gültig sind.
+	 */
+	public double bodyMassIndex() {
+		if (groesse != 0 && gewicht != 0) {
+			double quadrat = groesse * groesse;
+			double preBMI = gewicht / quadrat;
+			bmi = preBMI * 10000;
+			return bmi;
+		}
+		else {
+			System.out.println("##############");
+			System.out.println("Mindestens eine Angabe zum Berechnen des BMI fehlt.");
+			System.out.println("##############");
+			System.out.println("");
+			return 0;
+		}
+	}
+
+	/**
+	 * Gibt alle Werte auf der Konsole aus.
+	 */
+	public void detailAusgabe() {
+		bmi = bodyMassIndex();
+		System.out.println("##############");
+		System.out.println("Größe: " + groesse + "cm.");
+		System.out.println("Gewicht: " + gewicht + "kg.");
+		System.out.println("BMI: " + bmi);
+		System.out.println("##############");
+		System.out.println("");
 	}
 
 }
