8468b5eecbd3d8b9659c9882858655beb8b35266
Christoph Schüler <christoph.schueler@googlemail.com>
2009-10-01 18:05:38 +0200

    Intatial Commit: Heizungssteuerung
    Projekt schon fast fertig bearbeitet.

---
+++ Heizungssteuerung/Heizung.java
@@ -0,0 +1,56 @@
+
+/**
+ * .
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Heizung
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    double temperatur;
+
+    /**
+     * Konstruktor für Objekte der Klasse Heizung
+     */
+    public Heizung()
+    {
+        temperatur = 15.5;
+    }
+
+    /**
+     * Macht alles wärmer (siehe Klimawandel).
+     */
+    public void waermer()
+    {
+        temperatur += 0.5;
+    }
+    
+    /**
+     * Macht alles kühler (siehe Eiszwerg). 
+     */
+    public void kuehler()
+    {
+        temperatur -= 0.5;
+    }
+    
+    /**
+     * Macht alles total warm.
+     */
+    public void schnellWaermer()
+    {
+        temperatur += 5;
+    }
+    
+    /**
+     * Macht alles total kalt (macht im Kühlschrank das Licht aus, Freund vom Eiszwerg).
+     */
+    public void schnellKuehler()
+    {
+        // tragen Sie hier den Code ein
+        return y;
+    }
+    
+    
+    
+}
